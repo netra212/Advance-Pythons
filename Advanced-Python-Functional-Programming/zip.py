e23fa874a3c626739027826caa7630e6e2e4e998
netra2127 <netra200021kcbdr@gmail.com>
2024-10-04 23:56:48 +0100

    zip-functions

---
+++ Advanced-Python-Functional-Programming/zip.py
@@ -0,0 +1,8 @@
+# zip() : Pure function. 
+# Two iterable can be zipped together or zipped the item together.
+
+my_list = [1,2,3]
+your_list = [10, 20, 30]
+
+print(list(zip(my_list, your_list)))
+
