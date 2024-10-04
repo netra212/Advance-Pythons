8a166e95f84f6b6d05cb9c5980603ccb5640b521
netra2127 <netra200021kcbdr@gmail.com>
2024-10-04 23:58:09 +0100

    zip-pure function

--- Advanced-Python-Functional-Programming/zip.py
+++ Advanced-Python-Functional-Programming/zip.py
@@ -3,6 +3,8 @@
 
 my_list = [1,2,3]
 your_list = [10, 20, 30]
+their_list = (5, 4, 3)
 
-print(list(zip(my_list, your_list)))
+print(list(zip(my_list, your_list, their_list)))
 
+# zip iterates through each of them & zipped together.
