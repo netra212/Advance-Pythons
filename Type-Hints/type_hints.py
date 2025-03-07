2df0b8ece0ddc5c93187c065ace75096ee1552e6
netr212 <netra200021kcbdr@gmail.com>
2025-03-07 13:02:42 +0000

    adding comment on type hints

--- Type-Hints/type_hints.py
+++ Type-Hints/type_hints.py
@@ -5,6 +5,7 @@ def calculate_total_per_person(meal_price, number_of_people, tip=0.2):
     per_person_price = price_after_tip / number_of_people
     return per_person_price
 
+# Type hint does not effect the how program actual runs, this is only for the convinents.
 def calculate_total_per_person(meal_price: float, number_of_people: int, tip: float=0.2) -> float:
     price_after_tip: float = meal_price * tip + meal_price
     per_person_price: float = price_after_tip / number_of_people
