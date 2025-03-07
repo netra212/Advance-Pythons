1fe5ac2c1d97df72e55f2855b3dc4a3a760856f5
netr212 <netra200021kcbdr@gmail.com>
2025-03-07 14:45:39 +0000

    Interace in python

---
+++ Advanced-Python-OOPS/interface.py
@@ -0,0 +1,25 @@
+# Interface: Only define input and output of the thing but they don't specifiy the things. 
+# Abstract class: Define the input and output of the things, but they also provide an implementations. 
+# Generally, Interface and Abstract class cannot be instantiates on their own or are not able to create their own object. 
+
+class Duck:
+    def quack(self):
+        print("Duck Quacks")
+
+class Cat:
+    def meow(self):
+        print("Cat meows")
+
+counter = 0
+
+def make_it_quack(duck):
+    global counter
+
+    if counter > 0:
+        duck.quack()
+    else:
+        counter += 1
+
+if __name__ == "__main__":
+    cat = Cat()
+    cat.meow()
