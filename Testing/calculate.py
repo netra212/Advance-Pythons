26303368d1ba11eb0398b7acf707cbd9d1f427e2
netr212 <netra200021kcbdr@gmail.com>
2025-03-07 13:32:30 +0000

    calcualte for testing

---
+++ Testing/calculate.py
@@ -0,0 +1,16 @@
+
+# Testing Python Code.
+def add(x, y):
+    return x + y
+
+def substract(x, y):
+    return x - y
+
+def multiply(x, y):
+    return x * y
+
+def divide(x, y):
+    if x / y == 0:
+        ValueError()
+        
+    return x / y
